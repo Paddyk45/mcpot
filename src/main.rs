93d9626e1651516e5e2b0a525158ffeb63d5383b
patrick <patrickk0@tutanota.com>
2023-11-18 11:49:21 +0100

    Clippy warnings fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![warn(
+clippy::all,
+clippy::pedantic,
+clippy::nursery,
+)]
+
 use std::env;
 use std::process::exit;
 
@@ -11,7 +17,6 @@ use azalea_protocol::packets::status::clientbound_status_response_packet::{Clien
 use azalea_protocol::packets::status::ServerboundStatusPacket;
 use lazy_static::lazy_static;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::select;
 use tokio::signal::unix::{signal, SignalKind};
 
 use crate::config::Config;
@@ -27,12 +32,11 @@ async fn listener(addr: String, port: u16) {
     loop {
         match listener.accept().await {
             Ok((conn, addr)) => {
-
-                println!("Opening connection to {}", addr);
+                println!("Opening connection to {addr}");
                 tokio::spawn(async { handler(conn).await });
             }
             Err(why) => {
-                eprintln!("I/O Error while accepting client: {}", why)
+                eprintln!("I/O Error while accepting client: {why}");
             }
         }
     }
@@ -44,26 +48,23 @@ async fn handler(stream: TcpStream) -> color_eyre::Result<()> {
     let mut connection: Connection<ServerboundHandshakePacket, ClientboundHandshakePacket> = Connection::wrap(stream);
     let ServerboundHandshakePacket::ClientIntention(packet) = connection.read().await.expect("Failed to read packet");
     println!(
-        "Handshake from {} -> {}:{}, version={}, intention={:?}",
-        peer_addr,
+        "Handshake from {peer_addr} -> {}:{}, version={}, intention={:?}",
         packet.hostname,
         packet.port,
         packet.protocol_version,
         packet.intention
     );
+
     match packet.intention {
         ConnectionProtocol::Status => {
             let mut connection = connection.status();
             while let Ok(packet) = connection.read().await {
                 match packet {
                     ServerboundStatusPacket::StatusRequest(_) => {
-                        println!(
-                            "Got status request from {}",
-                            peer_addr
-                        );
+                        println!("Got status request from {peer_addr}");
                         let mut sample = vec![];
-                        for player in CONFIG.clone().player.unwrap_or(vec![]) {
-                            sample.push(SamplePlayer {id: player.uuid, name: player.name})
+                        for player in CONFIG.clone().player.unwrap_or_default() {
+                            sample.push(SamplePlayer {id: player.uuid, name: player.name});
                         }
                         connection.write(ClientboundStatusResponsePacket {
                             description: CONFIG.server.description.clone().into(),
@@ -79,10 +80,7 @@ async fn handler(stream: TcpStream) -> color_eyre::Result<()> {
                     }
 
                     ServerboundStatusPacket::PingRequest(pr) => {
-                        println!(
-                            "Got ping request from {}",
-                            peer_addr
-                        );
+                        println!("Got ping request from {peer_addr}",);
                         connection.write(ClientboundPongResponsePacket {
                             time: pr.time,
                         }.get()).await?;
@@ -96,8 +94,7 @@ async fn handler(stream: TcpStream) -> color_eyre::Result<()> {
             let packet = connection.read().await?;
             if let ServerboundLoginPacket::Hello(hi) = packet {
                 println!(
-                    "Got login from {}, name={}, uuid={:?}",
-                    peer_addr,
+                    "Got login from {peer_addr}, name={}, uuid={:?}",
                     hi.name,
                     hi.profile_id
                 );
